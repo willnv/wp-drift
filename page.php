f2e4cf8eea3f402fbc8c146f1b2b232eb6967f1d
Elian <elian.torquato@gmail.com>
2018-06-15 11:25:23 -0300

    ateração amadora do wilu

--- page.php
+++ page.php
@@ -1,12 +1,9 @@
 <?php get_header(); ?>
 
 <?php 
-    $query = new WP_Query(array(
-        'post_type' => 'page'
-    ));
 
-    if ( $query->have_posts() ):
-        while ( $query->have_posts() ): $query->the_post();
+    if ( have_posts() ):
+        while ( have_posts() ): the_post();
             the_content();
         endwhile;
     endif;
