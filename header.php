ce13fb41c46b4cfbf0311edaea1195868cc0f71b
Willon <willonnava@gmail.com>
2018-06-26 22:47:19 -0300

    revertendo isso

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-testeeeeeeeee
+
 <body <?php body_class() ?>>
     <header>
         <nav class="menu-principal" role="navigation"><?php wp_nav_menu( 'Menu Principal' ); ?></nav>
