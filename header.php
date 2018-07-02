3c76b29fcd2e565535379336c19cf291e1de662e
endrigocosta <40770280+endrigocosta@users.noreply.github.com>
2018-07-02 15:50:08 -0300

    arrumei as fonts no header

--- header.php
+++ header.php
@@ -4,6 +4,32 @@
         define( 'SITEURL', get_site_url() );
     ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style type="text/css">
+        @font-face {
+            font-family: "Thin";
+            src: url(<?= SITEURL ?>/wp-content/fontes/thin.otf);
+        }
+        @font-face {
+            font-family: "Light";
+            src: url(<?= SITEURL ?>/wp-content/fontes/light.otf);
+        }
+        @font-face {
+            font-family: "Regular";
+            src: url(<?= SITEURL ?>/wp-content/fontes/regular.otf);
+        }
+        @font-face {
+            font-family: "Medium";
+            src: url(<?= SITEURL ?>/wp-content/fontes/media.otf);
+        }
+        @font-face {
+            font-family: "Bold";
+            src: url(<?= SITEURL ?>/wp-content/fontes/bold.otf);
+        }
+        @font-face {
+            font-family: "Letra";
+            src: url(<?= SITEURL ?>/wp-content/fontes/letra.ttf);
+        }
+    </style>
 </head>
 
 <body <?php body_class() ?>>
