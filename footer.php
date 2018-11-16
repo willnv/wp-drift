37d411904a2c145f5e5575eb26b116e9ac03b62a
Willon <willonnava@gmail.com>
2018-11-16 09:33:38 -0200

    tag html com lang

--- footer.php
+++ footer.php
@@ -1,27 +1,28 @@
-<footer>
+	<footer>
 
-	<?php
-	/**
-	 * O tema foi feito para ser limpo e de boa
-	 * manutenibilidade, então utilize esse
-	 * espaço para escrever o footer em HTML puro!
-	*/
-	?>
+		<?php
+		/**
+		 * O tema foi feito para ser limpo e de boa
+		 * manutenibilidade, então utilize esse
+		 * espaço para escrever o footer em HTML puro!
+		*/
+		?>
 
-	<div class="subfooter">
-		<div class="dft-grid">
-			<div>
-				<span>&copy; Empresa Fictícia. Todos os direitos Reservados.</span>
-			</div>
-			<div>
-				<span>Desenvolvido por <a target="_blank" style="color:#0f0" href="https://driftweb.com.br/" rel="noopener">Drift Web</a></span>
+		<div class="subfooter">
+			<div class="dft-grid">
+				<div>
+					<span>&copy; Empresa Fictícia. Todos os direitos Reservados.</span>
+				</div>
+				<div>
+					<span>Desenvolvido por <a target="_blank" style="color:#0f0" href="https://driftweb.com.br/" rel="noopener">Drift Web</a></span>
+				</div>
 			</div>
 		</div>
+
+	</footer>
+
+	<div class="scripts">
+		<?php wp_footer() ?>
 	</div>
-
-</footer>
-
-<div class="scripts">
-	<?php wp_footer() ?>
-</div>
-</body>
+	</body>
+</html>
--- header.php
+++ header.php
@@ -1,44 +1,45 @@
 <!DOCTYPE html>
-<head>
-    <?php 
-        wp_head(); 
-        define( 'SITEURL', get_site_url() );
-    ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style type="text/css">
-        @font-face {
-            font-family: "Thin";
-            src: url(<?= SITEURL ?>/wp-content/fontes/thin.otf);
-        }
-        @font-face {
-            font-family: "Light";
-            src: url(<?= SITEURL ?>/wp-content/fontes/light.otf);
-        }
-        @font-face {
-            font-family: "Regular";
-            src: url(<?= SITEURL ?>/wp-content/fontes/regular.otf);
-        }
-        @font-face {
-            font-family: "Medium";
-            src: url(<?= SITEURL ?>/wp-content/fontes/media.otf);
-        }
-        @font-face {
-            font-family: "Bold";
-            src: url(<?= SITEURL ?>/wp-content/fontes/bold.otf);
-        }
-        @font-face {
-            font-family: "Letra";
-            src: url(<?= SITEURL ?>/wp-content/fontes/letra.ttf);
-        }
-    </style>
-</head>
+<html lang="pt">
+    <head>
+        <?php 
+            wp_head(); 
+            define( 'SITEURL', get_site_url() );
+        ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+            <style type="text/css">
+            @font-face {
+                font-family: "Thin";
+                src: url(<?= SITEURL ?>/wp-content/fontes/thin.otf);
+            }
+            @font-face {
+                font-family: "Light";
+                src: url(<?= SITEURL ?>/wp-content/fontes/light.otf);
+            }
+            @font-face {
+                font-family: "Regular";
+                src: url(<?= SITEURL ?>/wp-content/fontes/regular.otf);
+            }
+            @font-face {
+                font-family: "Medium";
+                src: url(<?= SITEURL ?>/wp-content/fontes/media.otf);
+            }
+            @font-face {
+                font-family: "Bold";
+                src: url(<?= SITEURL ?>/wp-content/fontes/bold.otf);
+            }
+            @font-face {
+                font-family: "Letra";
+                src: url(<?= SITEURL ?>/wp-content/fontes/letra.ttf);
+            }
+        </style>
+    </head>
 
-<body <?php body_class(); ?>>
-    <header class="header-principal">
-        <a href='<?= SITEURL ?>'><?php the_custom_logo(); ?></a>
-        <div>
-            <nav class="menu-principal" role="navigation">
-                <?php wp_nav_menu( array ( 'menu' => 'Menu Principal' ) ); ?>
-            </nav>
-        </div>
-    </header>
+    <body <?php body_class(); ?>>
+        <header class="header-principal">
+            <a href='<?= SITEURL ?>'><?php the_custom_logo(); ?></a>
+            <div>
+                <nav class="menu-principal" role="navigation">
+                    <?php wp_nav_menu( array ( 'menu' => 'Menu Principal' ) ); ?>
+                </nav>
+            </div>
+        </header>
