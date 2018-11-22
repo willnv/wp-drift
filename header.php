faf3698bb7a65f9455c5ba854859714eefbd86ba
Willon <willonnava@gmail.com>
2018-11-22 14:27:42 -0200

    minimum-scale=1 evita problemas com pos fixed no mobile e não desabilita o zoom do user

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="pt-BR">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
         <meta name="theme-color" content="<?= COR_BARRA_NAVEGADOR ?>">
 
         <?php if ( SUPORTE_PWA ) { ?>
