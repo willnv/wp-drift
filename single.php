adccac107a72d36a36cf4bee3741270dd1eb6009
Willon <willonnava@gmail.com>
2019-01-07 17:00:08 -0200

    adicionei algumas tags faltando, e arrumei o loop

--- single.php
+++ single.php
@@ -1,29 +1,25 @@
 <?php 
 
-    /**
-     *  Template usado para o single post
-     */
-    get_header();
-?>
+/**
+ *  Template usado para o single post
+ */
+get_header();
 
-<div style="background-image:url('<?= get_the_post_thumbnail_url(); ?>');" class="page-title-single">
-    <div class="main-grid">
-       <div class="titulo-single">
-           <?php the_title(); ?>
-       </div>
+while ( have_posts() ) : the_post(); ?>
 
-       <div class="single-date">
-           <?php the_date(); ?>
-       </div>
-   </div>
-</div>
-
-<section class="content-single-post main-content">
-    <div class="main-grid">
-        <?php while ( have_posts() ) : the_post();
-            the_content();
-        endwhile; ?>
+    <div style="background-image:url('<?= get_the_post_thumbnail_url(); ?>');" class="page-title-single">
+        <div class="main-grid">
+            <div class="titulo-single"><h1><?php the_title(); ?></h1></div>
+            <div class="single-date"><time><?php the_date(); ?></time></div>
+        </div>
     </div>
-</section>
+    <section class="content-single-post main-content">
+        <div class="main-grid">
+            <?php the_content(); ?>
+            <div class="data-modificado"><p>Modificado em: <time><?php the_modified_date(); ?></time></p></div>
+        </div>
+    </section>
+    
+<?php endwhile;
 
-<?php get_footer();
+get_footer();
