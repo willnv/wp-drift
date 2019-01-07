be9ed92bf715731bbfef93e35a18ea71e4519abf
Willon <willonnava@gmail.com>
2019-01-07 16:56:41 -0200

    melhorias e algumas classes a mais

--- footer.php
+++ footer.php
@@ -1,28 +1,16 @@
 	<footer class="main-footer">
-
-		<?php
-		/**
-		 * O tema foi feito para ser limpo e de boa
-		 * manutenibilidade, então utilize esse
-		 * espaço para escrever o footer em HTML.
-		*/
-		?>
-
 		<div class="subfooter">
 			<div class="main-grid">
 				<div>
-					<span>&copy; Empresa Fictícia. Todos os direitos Reservados.</span>
+					<span class="copyright">&copy; Empresa Fictícia. Todos os direitos Reservados.</span>
 				</div>
 				<div>
-					<span>Desenvolvido por <a target="_blank" style="color:#0f0" href="https://driftweb.com.br/" rel="noopener">Drift Web</a></span>
+					<span class="desenvolvimento">Desenvolvido por <a target="_blank" href="https://driftweb.com.br/" rel="nofollow noopener">Drift Web</a></span>
 				</div>
 			</div>
 		</div>
-
 	</footer>
 
-	<div class="scripts">
-		<?php wp_footer() ?>
-	</div>
+	<div class="scripts"><?php wp_footer(); ?></div>
 	</body>
 </html>
