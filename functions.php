b7ff6e36bc8a5c0d1bffc59d4c6e782ec20baab8
Willon <willonnava@gmail.com>
2019-01-07 17:04:56 -0200

    várias alterações, incluindo o Slick Slider e desativação de scripts desnecessários pro WP funcionar

--- functions.php
+++ functions.php
@@ -98,9 +98,54 @@ function dft_classes_extras_body( $classes ) {
 add_filter( 'body_class', 'dft_classes_extras_body' );
 
 
+/**
+ * Retorna o código do analytics, o ID deve
+ * ser definido no theme-config.php
+ */
+function dft_analytics_script() {
+
+    if ( !defined( 'ID_ANALYTICS' ) || !ID_ANALYTICS )
+        return; ?>
+        
+    <script async src="https://www.googletagmanager.com/gtag/js?id=<?= ID_ANALYTICS ?>"></script>
+    <script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '<?= ID_ANALYTICS ?>');</script>
+<?php
+}
+
 
 /**
- * Carregando Scripts e Styles
+ * Desativa scripts desnecessários em algumas páginas.
+ * 
+ * O script de emoji só será carregado se a global
+ * BLOG estiver true e se for um single-post. Em
+ * post types criados e páginas não será carregado.
+ */
+if ( !BLOG ) {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+} else {
+    add_action( 'wp_enqueue_scripts', 'desativa_script_emojis' );
+}
+
+function desativa_script_emojis() {
+    if ( is_page() || !is_singular( 'post' ) ) {
+        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+        remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    }
+}
+
+
+/**
+ * Desativa o script wp-embed.min.js
+ */
+function desativa_embed() {
+    wp_deregister_script( 'wp-embed' );
+}
+add_action( 'wp_enqueue_scripts', 'desativa_embed', 99 );
+
+
+/**
+ * Carrega Scripts e Styles
 */
 function dft_enqueue_scripts() {
 
@@ -111,7 +156,6 @@ function dft_enqueue_scripts() {
     // CSS
     wp_enqueue_style( 'dft-main-less', THEME_DIR . '/style.less' );
     wp_enqueue_style( 'dft-main-css', THEME_DIR . '/style.css' );
-
 
     wp_enqueue_script( 'animate-it-js', THEME_DIR . '/lib/animate-it/css3-animate-it.js', array( 'jquery' ) );
     wp_enqueue_style( 'animate-it-css', THEME_DIR . '/lib/animate-it/animations.css' );
@@ -132,6 +176,11 @@ function dft_enqueue_scripts() {
     if ( IMAGE_LIGHTBOX ) {
         wp_enqueue_script( 'lightbox-js', THEME_DIR . '/lib/lightbox/lightbox.min.js', array( 'jquery' ) );
         wp_enqueue_style( 'lightbox-css', THEME_DIR . '/lib/lightbox/lightbox.min.css' );
+    }
+
+    if ( SLICK_SLIDER ) {
+        wp_enqueue_style( 'slick-css', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css' );
+        wp_enqueue_script( 'slick-js', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js' );
     }
 }
 
