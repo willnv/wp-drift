f17e350c5cf23e9d3e4aa19fd15bb9a5aa76dee4
Willon <dev.willnv@gmail.com>
2021-05-04 01:14:38 -0400

    removed theme dir

--- theme-config.php
+++ theme-config.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * General config file for
+ * Config file for
  * theme setup
  */
 
@@ -11,21 +11,17 @@ define( 'IMAGE_LIGHTBOX', false );
 define( 'SLIM_SELECT', false );
 define( 'SLICK_SLIDER', false );
 
-
-//This is required if using Woocommerce
+// Using WooCommerce?
 define( 'WOOCOMMERCE_SUPPORT', false );
-
 
 // PWA / Mobile
 define( 'PWA_SUPPORT', true );
 define( 'TOP_BAR_COLOR', '#000000' );
 
-
 // Misc
 define( 'MAINTENANCE', false );
 define( 'BLOG', true );
 define( 'ID_ANALYTICS', false );
-define( 'THEME_ROOT', get_theme_root() . '/wp-drift' );
 
 // Debug
 ini_set( 'display_errors', 'On' );
